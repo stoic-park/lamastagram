c0fd22d18baf05e6630f922e5c4b7640ad27d382
stoic-park <demige79@gmail.com>
2020-09-15 06:37:11 +0900

    :wrench: Feat: firebase start
    - firebase.js 기본 세팅

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({});
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+
+export { db, auth, storage };
