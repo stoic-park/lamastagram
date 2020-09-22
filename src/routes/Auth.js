f007fd37a82f9ee17b85a2c62e1ea2fb04eccd37
stoic-park <demige79@gmail.com>
2020-09-22 23:15:24 +0900

    :fire: Feat: add email auth!
    - 추가적으로 이메일 인증 기능 추가중!

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -5,12 +5,29 @@ import firebase from "firebase";
 
 function Auth() {
   const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // email, password 변화된 스테이트를 동시에!
+  const onChange = (event) => {
+    // console.log(event.target.name);
+    // const {target: {name, value}} = event;
+    const { name, value } = event.target;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
   // console.log(username);
   // var provider = new auth.GoogleAuthProvider();
   var provider = new firebase.auth.GoogleAuthProvider();
 
   // sign-up function
-  const signUp = (event) => {
+  const signUpGoogle = (event) => {
     // 왜 쓰는지 알제?
     event.preventDefault();
 
@@ -28,7 +45,25 @@ function Auth() {
   return (
     <center>
       <div>
-        <button onClick={signUp}>GOOGLE</button>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            placeholder="Email"
+            required
+            value={email}
+            onChange={onChange}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            required={password}
+            onChange={onChange}
+          />
+          <input typ="submit" value="Log In" />
+        </form>
+        <div>
+          <button onClick={signUpGoogle}>Continue with GOOGLE</button>
+        </div>
       </div>
     </center>
   );
